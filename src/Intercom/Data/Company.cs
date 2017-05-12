05359ed2269c7aaef1f31037e07b76bcf0ac352d
Micky McQuade <micky@vintagesoftware.com>
2017-05-12 11:30:58 -0500

    fix (company): changed company.plan to string
    company.plan was a Plan object in this library. The API has it has a
    string value. Changed library to match API

--- src/Intercom/Data/Company.cs
+++ src/Intercom/Data/Company.cs
@@ -15,7 +15,7 @@ namespace Intercom.Data
 	{
 		public bool? remove { set; get; }
 		public string name { get; set; }
-		public Plan plan { get; set; }
+		public string plan { get; set; }
 		public string company_id { get; set; }
 		public long? remote_created_at { get; set; }
 		public long? created_at { get; set; }
