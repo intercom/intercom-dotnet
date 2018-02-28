70798bdbd50c399ca02a9cf39706a6e3b53d9c17
brunomartinspro <hello@brunomartins.pro>
2018-02-28 11:34:06 +0000

    Added User model to UserConversationMessage.From, this allows a more OOP way of dealing with From.

--- src/Intercom/Data/UserConversationMessage.cs
+++ src/Intercom/Data/UserConversationMessage.cs
@@ -18,23 +18,40 @@ namespace Intercom.Data
 			public String email { set; get; }
 			public String user_id { set; get; }
 
-            public From (String type = Message.MessageFromOrToType.USER, String id = null, String email = null, String user_id = null)
-			{
-				if (String.IsNullOrEmpty (type))
-					throw new ArgumentNullException (nameof(type));
+            public From(String type = Message.MessageFromOrToType.USER, String id = null, String email = null, String user_id = null, User user = null)
+            {
+                //Validate type of message
+                if (String.IsNullOrEmpty(type))
+                    throw new ArgumentNullException(nameof(type));
 
-				if (String.IsNullOrEmpty (id) && String.IsNullOrEmpty (email) && String.IsNullOrEmpty (user_id))
-					throw new ArgumentException ("you need to provide either 'id', 'user_id', 'email' to view a user.");
+                //Validate required fields related to User
+                if (String.IsNullOrEmpty(id) && String.IsNullOrEmpty(email) && String.IsNullOrEmpty(user_id))
+                    throw new ArgumentException("you need to provide either 'id', 'user_id', 'email' to view a user.");
 
+                //Validate User required fields
+                if (user != null && String.IsNullOrEmpty(user.id) && String.IsNullOrEmpty(user.email) && String.IsNullOrEmpty(user.user_id))
+                    throw new ArgumentException("you need to provide either 'id', 'user_id', 'email' to view a user.");
+
+                //Validate required types
                 if (type != Message.MessageFromOrToType.USER && type != Message.MessageFromOrToType.CONTACT)
-					throw new ArgumentException ("'type' value must be either 'contact' or 'user'.");
+                    throw new ArgumentException("'type' value must be either 'contact' or 'user'.");
 
-				this.id = id;
-				this.email = email;
-				this.user_id = user_id;
-				this.type = type;
-			}
-		}
+                if (user != null)
+                {
+                    this.id = user.id;
+                    this.email = user.email;
+                    this.user_id = user.user_id;
+                }
+                else
+                {
+                    this.id = id;
+                    this.email = email;
+                    this.user_id = user_id;
+                }
+
+                this.type = type;
+            }
+        }
 
 		public From from { set; get; }
 
