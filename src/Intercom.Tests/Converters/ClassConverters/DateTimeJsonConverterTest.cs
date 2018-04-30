5ebeebffa867f85ccd69ea9dd0712fef70bd3d67
Josh Johnson <josh.johnson@truecommerce.com>
2018-04-30 10:48:16 -0400

    Implement ReadJson()

--- src/Intercom.Tests/Converters/ClassConverters/DateTimeJsonConverterTest.cs
+++ src/Intercom.Tests/Converters/ClassConverters/DateTimeJsonConverterTest.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.IO;
 
 using Intercom.Test;
+using Newtonsoft.Json;
 using NUnit.Framework;
+
+using Intercom.Converters.ClassConverters;
 
 namespace Intercom.Tests.Converters.ClassConverters
 {
     [TestFixture]
     public class DateTimeJsonConverterTest : TestBase
     {
-        [Test]
-        public void ReadJson_ForDateTime_ReturnsValidDateTime()
+        private readonly DateTimeJsonConverter _converter;
+
+        public DateTimeJsonConverterTest()
         {
-            throw new NotImplementedException();
+            _converter = new DateTimeJsonConverter();
+        }
+
+        [TestCase("608688900")]
+        public void ReadJson_ForDateTime_ReturnsValidDateTime(string json)
+        {
+            using (StringReader stringReader = new StringReader(json))
+            using (JsonReader jsonReader = new JsonTextReader(stringReader))
+            {
+                DateTime dateCreated = (DateTime)_converter.ReadJson(jsonReader, typeof(DateTime), null, null);
+
+                Assert.AreEqual(new DateTime(1989, 4, 16, 0, 15, 0), dateCreated);
+            }
         }
 
         [Test]
---
+++ src/Intercom/Converters/ClassConverters/DateTimeJsonConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace Intercom.Converters.ClassConverters
+{
+    public class DateTimeJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.Equals(typeof(DateTime));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            long unixTimestamp;
+
+            try
+            {
+                unixTimestamp = Convert.ToInt64(reader.ReadAsDouble());
+            }
+
+            catch (InvalidCastException ex)
+            {
+                throw new FormatException("Dates must be represented as UNIX timestamps in JSON.", ex);
+            }
+
+            DateTime unixEpoch = new DateTime(1970, 1, 1);
+            DateTime result = unixEpoch.AddSeconds(unixTimestamp);
+
+            return result;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
