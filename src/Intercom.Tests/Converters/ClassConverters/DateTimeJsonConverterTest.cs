ba6b2526ab8463fa49d1e24bf6c87248e1ef996d
Josh Johnson <josh.johnson@truecommerce.com>
2018-04-30 10:18:41 -0400

    Add Failing Tests

---
+++ src/Intercom.Tests/Converters/ClassConverters/DateTimeJsonConverterTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Intercom.Test;
+using NUnit.Framework;
+
+namespace Intercom.Tests.Converters.ClassConverters
+{
+    [TestFixture]
+    public class DateTimeJsonConverterTest : TestBase
+    {
+        [Test]
+        public void ReadJson_ForDateTime_ReturnsValidDateTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void WriteJson_ForDateTime_ReturnsValidUnixTimestamp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
