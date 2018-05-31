107fc3512de930f8058286f6336c5266deb2cd8a
Thomas Ardal <thomasardal@gmail.com>
2018-05-31 07:35:36 +0200

    Include count on segment

--- src/Intercom/Data/Segment.cs
+++ src/Intercom/Data/Segment.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using Intercom.Core;
-using Intercom.Data;
-
-
-using Intercom.Clients;
-
-using Intercom.Exceptions;
-
+﻿using Intercom.Core;
 
 namespace Intercom.Data
 {
-	public class Segment : Model
-	{
+    public class Segment : Model
+    {
         public string name { get; set; }
         public long created_at { get; set; }
         public long updated_at { get; set; }
+        public int? count { get; set; }
 
-		public Segment ()
-		{
-		}
-	}
+        public Segment()
+        {
+        }
+    }
 }
 
