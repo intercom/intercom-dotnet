58026a28004975758fa991d5a37a4de3ba359a6c
Joe <joseph.rall@live.com>
2018-11-01 13:14:25 -0400

    Added some tests to ensure Replys return conversation objects and not parts.

--- src/Intercom.Tests/Data/UserConversationReplyTest.cs
+++ src/Intercom.Tests/Data/UserConversationReplyTest.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Intercom.Clients;
+using Intercom.Core;
+using Intercom.Data;
+using NUnit.Framework;
 
 namespace Intercom.Test
 {
-    // TODO: write tests for UserConversationReply 
-    public class UserConversationReplyTest
+    [TestFixture]
+    public class UserConversationReplyTest : TestBase
     {
         public UserConversationReplyTest()
         {
+        }
+
+        [Test]
+        public void UserConverstationReply()
+        {
+            var mock = BuildSuccessMockClient<UserConversationsClient, Conversation>("Conversation.json", new Authentication(AppId, AppKey));
+
+            var convo = mock.Reply(new UserConversationReply("147", "We noticed you using our Product,  do you have any questions?", "536e564f316c83104c000020"));
+
+            Assert.IsNotNull(convo);
+            Assert.IsTrue(convo.conversation_message.body.Contains("We noticed you using our Product"));
+        }
+
+        [Test]
+        public void ListUserConversations()
+        {
+            var mock = BuildSuccessMockClient<UserConversationsClient, Conversation>("ConversationsList.json", new Authentication(AppId, AppKey));
+
+            var convo = mock.List("536e564f316c83104c000020");
+
+            Assert.IsNotNull(convo);
+            Assert.AreEqual(1, convo.conversations.Count);
         }
     }
 }
