8592d9bd7a4066ad17724d2ba0c8797350d90865
Ash Lawson <ashley.lawson@white-springs.com>
2020-05-16 00:35:10 +0100

    Fix paginated contact list deserialization

---
+++ src/Intercom/Data/Next.cs
@@ -0,0 +1,8 @@
+﻿namespace Intercom.Data
+{
+	public class Next
+	{
+		public int page { get; set; }
+		public string starting_after { get; set; }
+	}
+}
--- src/Intercom/Data/Pages.cs
+++ src/Intercom/Data/Pages.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using Intercom.Core;
-using Intercom.Data;
-
-
-using Intercom.Clients;
-
-using Intercom.Exceptions;
-
+﻿using Intercom.Core;
 
 namespace Intercom.Data
 {
 	public class Pages : Model
 	{
-		public string next { get; set; }
+		public Next next { get; set; }
 		public int page { get; set; }
 		public int per_page { get; set; }
 		public int total_pages { get; set; }
